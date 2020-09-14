c685d1aef2a4dce6db4341adcdc4b06851aa54a8
Jaewoo Kang <timg5599@bu.edu>
2020-09-14 17:18:43 +0900

    worked on outdoor(lake/park)

--- 01-python/outdoors/park.py
+++ 01-python/outdoors/park.py
@@ -1,3 +1,12 @@
 def draw_park():
-    print("park not found")
+    print("        ▲                               ")
+    print("       ▲▲      ▲                      ")
+    print("      ▲▲▲    ▲▲     ▲                ")
+    print("      ▲▲▲   ▲▲▲   ▲▲                ")
+    print("     ▲▲▲▲ ▲▲▲▲ ▲▲▲       O       ")
+    print("       |  |    |   |  ▲▲▲▲    / l |         ")
+    print("       |  |    |   |   |   |        l       ")
+    print("       |  |    |   |   |   |       / |        ")
+    print("       |  |    |   |   |   |      /  |        ")
+    print("______________________________________________")
     return
